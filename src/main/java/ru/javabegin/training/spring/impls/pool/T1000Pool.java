0c24d1cdbaf769d43e8bf931a675d3e86d52319d
dzuiev <dzuiev@playtika.com>
2019-04-21 03:33:48 +0300

    Collections Map

--- src/main/java/ru/javabegin/training/spring/impls/pool/T1000Pool.java
+++ src/main/java/ru/javabegin/training/spring/impls/pool/T1000Pool.java
@@ -1,26 +1,33 @@
 package ru.javabegin.training.spring.impls.pool;
 
 import java.util.Collection;
+import java.util.Map;
 
 import ru.javabegin.training.spring.interfaces.Robot;
 import ru.javabegin.training.spring.interfaces.RobotPool;
 
 public class T1000Pool implements RobotPool {
 
-    private Collection<Robot> robotCollection;
+    private Map<String, Robot> robotCollection;
 
-    public void setRobotCollection(Collection<Robot> robotCollection){
+    public T1000Pool(Map<String, Robot> robotCollection){
+        super();
         this.robotCollection = robotCollection;
     }
 
-    @Override
-    public Collection<Robot> getRobotCollection() {
-        return null;
+    public void setRobotCollection(Map<String, Robot> robotCollection) {
+        this.robotCollection = robotCollection;
     }
 
     public void action() {
-        for (Robot robot : robotCollection) {
-            robot.action();
+        for (Map.Entry<String, Robot> entry : robotCollection.entrySet()) {
+            System.out.println(entry.getKey());
+            entry.getValue().action();
         }
+    }
+
+    @Override
+    public Map<String, Robot> getRobotCollection() {
+        return null;
     }
 }
--- src/main/java/ru/javabegin/training/spring/interfaces/RobotPool.java
+++ src/main/java/ru/javabegin/training/spring/interfaces/RobotPool.java
@@ -1,8 +1,9 @@
 package ru.javabegin.training.spring.interfaces;
 
 import java.util.Collection;
+import java.util.Map;
 
 public interface RobotPool {
 
-    Collection<Robot> getRobotCollection();
+    Map<String, Robot> getRobotCollection();
 }
